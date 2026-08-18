bd1cfe0c0ff4280dd0fcc1ad7abc9937b8a0d1e9
alchemistnick <91359051+alchemistnick@users.noreply.github.com>
2026-08-18 10:52:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import requests
 import streamlit as st
 
-WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzt19tpSOAd8HVEW6q8CU5-d9sK9H7jKGnmgI23HVW-aJ3IIqLDL2M3IiAJcwhTCaU0YQ/exec"
+WEBAPP_URL = "https://script.google.com/macros/s/AKfycbweJdBRjTIkzj8YJCIA5knhwR3JNt_NySHv-7akhr_51l_SIcAX0emNaXrgD7hul6ZdFQ/exec"
 SHEET_ID_EVALS = "1V5rWEolARQ3PlZTbVrrhEWUc7bipJF0t2iMznxjvKgk"
 
 CARACTERES_SEGUROS = "BCDFGHJKLMNPQRSTVWXYZ0123456789"
