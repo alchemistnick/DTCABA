deaeced162929867d738e5e22d4591af311f8b8a
alchemistnick <91359051+alchemistnick@users.noreply.github.com>
2026-09-13 13:32:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,14 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded",
 )
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    header {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # ---------------------------------------------------------
 # ESTILOS ADAPTABLES (MODO CLARO Y MODO OSCURO)
